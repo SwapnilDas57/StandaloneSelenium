60a38c4cbd708e88e0757db162c4cf0e6e374c97
Swapnil Das <swpnlds38@gmail.com>
2025-05-06 00:17:41 +0530

    Adding

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7b/40c9414ee12900101f13e8f6ca539e47
@@ -0,0 +1,6 @@
+public class TestBase {
+	
+	public static final String CONFIGPATH="src/test/resources/config.properties";
+	
+	
+}
--- CucumberBDD/src/main/java/TestBase.java
+++ CucumberBDD/src/main/java/TestBase.java
@@ -2,5 +2,5 @@ public class TestBase {
 	
 	public static final String CONFIGPATH="src/test/resources/config.properties";
 	
-	
+	//test
 }
